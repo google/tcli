bf3b758583221b19cf3fd726c48e507992da00d9
Daniel Harrison <13085330+harro@users.noreply.github.com>
2025-01-09 19:02:38 +1100

    Fix setup.py (#48)
    * Fixed absl name in setup.py
    
    * Use install_requires rather than requires in setup.py

--- setup.py
+++ setup.py
@@ -47,11 +47,13 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         "Programming Language :: Python :: 3 :: Only",],
-    requires=['absl-py', 'textfsm', 'tqdm'],
-    packages=['tcli'],
+    install_requires=['absl-py', 'textfsm', 'tqdm',],
+    packages=['tcli',],
     python_requires='>=3.6, <4',
     include_package_data=True,
     package_data={
-        "tcli.testdata": ["**/*"],
+        'tcli.testdata': ['*'],
+        'tcli.testdata.device_output': ['*'],
     },
     )
+
